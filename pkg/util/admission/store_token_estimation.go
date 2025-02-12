654c521ead26d487bcc5ee7b8ebf09430f44a39b
Aaditya Sondhi <20070511+aadityasondhi@users.noreply.github.com>
2025-02-12 12:10:22 -0500

    wip

--- pkg/util/admission/store_token_estimation.go
+++ pkg/util/admission/store_token_estimation.go
@@ -111,6 +111,8 @@ const ingestMultiplierMin = 0.5
 const ingestMultiplierMax = 1.5
 const writeAmpMultiplierMin = 1.0
 const writeAmpMultiplierMax = 100.0
+const writeIOPSMultiplierMin = 1.0 / (256 * 1024) // 256KiB sequential write can be 1 IO operation.
+const writeIOPSMultiplierMax = 1.0                // Worst case scenario is that 1B written will use up 1 IO operation on its own.
 
 // 256KiB sequential write can be 1 IO operation.
 const writeIOPSMultiplierMin = 1.0 / (256 * 1024)
